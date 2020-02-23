f696864e47dab7ea003054d00966e05adf795ddb
Roberto <email@rmineoweb.com>
2020-02-23 21:51:27 +0100

    Initial Commit

---
+++ My first xcode app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My first xcode app
+//
+//  Created by Ale&Roby on 23/02/20.
+//  Copyright © 2020 Ale&Roby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
